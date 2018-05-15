9c3c4aeecd5a9641a0e81a0657c27e7e9e09c53f
Eunicio <euniciodealberito.cun@hotmail.com>
2018-05-15 11:30:23 +0200

    Commit 1

---
+++ armazemww/src/main/java/com/armazemww/ArmazemwwApplication.java
@@ -0,0 +1,12 @@
+package com.armazemww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmazemwwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmazemwwApplication.class, args);
+	}
+}
